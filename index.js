0c9cb995270e0e59ea7db6840b3a3a29e39d85e9
dev1 <admin@atgenes.com>
2018-07-03 17:26:12 +0700

    Begin Blog by

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser')
+var Realm = require('realm')
+
+var app = express();
+
+let PostSchema = {
+    name: 'Post',
+    properties: {
+      timestamp: 'date',
+      title: 'string',
+      content: 'string'
+    }
+};
+  
+var blogRealm = new Realm({
+    path: 'blog.realm',
+    schema: [PostSchema]
+});
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    let posts = blogRealm.objects('Post').sorted('timestamp', true);
+    res.render('index.ejs', {posts: posts});
+});
+
+app.get('/write',function(req, res){
+    res.sendFile(__dirname + "/views/write.html");
+})
+
+app.post('/write', function(req,res){
+    let title = req.body['title'],
+    content = req.body['content'],
+    timestamp = new Date();
+    blogRealm.write(() => {
+        blogRealm.create('Post', {title: title, content: content, timestamp: timestamp});
+    });
+  res.sendFile(__dirname + "/views/write-complete.html");
+})
+
+app.listen(8000, function(){
+    console.log('Go!');
+})
